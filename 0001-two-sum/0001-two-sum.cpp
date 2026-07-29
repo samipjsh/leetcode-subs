c6a5764a0f3cd162db6b91c15f974cc55d2e1a0a
samipjsh <samipjsh2064@gmail.com>
2026-07-29 21:42:32 +0545

    Time: 0 ms (100%), Space: 14.9 MB (39.55%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
+
+        unordered_map<int,int> reverse;
+
+        for (int i=0;i<nums.size();i++){
+            int rev=target-nums[i];
+
+            if(reverse.find(rev)!=reverse.end(rev)) return {reverse[rev],i};
+
+            reverse[nums[i]] = i;
+        }
+        return {};
     }
 };
